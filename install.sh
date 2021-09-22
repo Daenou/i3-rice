f54690b28e3c176df73a30eb055ae6a98a078cd5
Daenou <29140988+Daenou@users.noreply.github.com>
2021-09-22 22:15:35 +0200

    Ubuntu 20.04 update (#3)
    * fix $XRSC path
    
    * Remove gpmdp mentions
    
    * Added X11 touchpad conf and removed old stuff
    
    * various improvements to media handling
    
    * Added volume i3block with pulsemixer
    
    * Added actual words to the readme
    
    * Added install instructions to readme

--- install.sh
+++ install.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
 
+if [ -d "./files" ]
+then
+  cd ./files
+else
+  echo "ERROR: ./files directory doesn't exist, are you executing this script from the right path?"
+  exit 1
+fi
 
 echo "Installing prerequisites..."
-sudo apt-get -y install i3 i3blocks i3lock rofi compton jq scrot
+sudo apt-get -y install i3 i3blocks i3lock rofi compton jq scrot brightnessctl redshift pulsemixer
+sudo snap install indicator-sound-switcher
 
-echo "Creating symbolic link for Google Play Music Desktop Player..."
-
-GPPATH="$HOME/.config/gpmdp"
 I3CFG="$HOME/.config/i3/config"
 I3BCFG="$HOME/.config/i3/i3blocks.conf"
 XRSC="$HOME/.Xresources"
 FONTPATH="$HOME/.fonts"
 SCRIPTPATH="$HOME/scripts"
-
-if ! [ -L "$GPPATH" ]
-then
-  echo "Creating symbolic link for gpmdp..."
-  ln --symbolic "$HOME/.config/Google Play Music Desktop Player/" "$HOME/.config/gpmdp/"
-else
-  echo "Symbolic link already exists..."
-fi
-
+X11CFGDIR="/etc/X11/xorg.conf.d/"
 
 if ! [ -e "$I3CFG" ]
 then
   echo "Copying i3 config file..."
-  cp ./.config/i3/config "$I3CFG"
+  cp ./config "$I3CFG"
 else
   while true; do
     read -p "An i3 config already exists, overwrite? [y,n]: " yn
     case $yn in
-      [Yy]) cp ./.config/i3/config "$I3CFG"; break;;
+      [Yy]) cp ./config "$I3CFG"; break;;
       [Nn]) break;;
       *) echo "Please answer 'y' or 'n'."
     esac
@@ -40,12 +37,12 @@ fi
 if ! [ -e "$I3BCFG" ]
 then
   echo "Copying i3-Blocks config file..."
-  cp ./.config/i3/i3blocks.conf "$I3BCFG"
+  cp ./i3blocks.conf "$I3BCFG"
 else
   while true; do
     read -p "An i3-Blocks config already exists, overwrite? [y,n]: " yn
     case $yn in
-      [Yy]) cp ./.config/i3/i3blocks.conf "$I3BCFG"; break;;
+      [Yy]) cp ./i3blocks.conf "$I3BCFG"; break;;
       [Nn]) break;;
       *) echo "Please answer 'y' or 'n'."
     esac
@@ -55,17 +52,39 @@ fi
 if ! [ -e "$XRSC" ]
 then
   echo "Copying Xresources config file..."
-  cp ./Xresources "$I3BCFG"
+  cp ./Xresources "$XRSC"
 else
   while true; do
     read -p "An Xresources config already exists, overwrite? [y,n]: " yn
     case $yn in
-      [Yy]) cp ./Xresources "$I3BCFG"; break;;
+      [Yy]) cp ./Xresources "$XRSC"; break;;
       [Nn]) break;;
       *) echo "Please answer 'y' or 'n'."
     esac
   done
 fi
+
+if ! [ -d "$X11CFGDIR" ]
+then
+  echo "Creating $X11CFGDIR and copying touchpad config file..."
+  mkdir "$X11CFGDIR"
+  cp ./90-touchpad.conf "$X11CFGDIR"
+else
+  if ! [ -e "$X11CFGDIR/90-touchpad.conf" ]
+  then
+    cp ./90-touchpad.conf "$X11CFGDIR"
+  else
+    while true; do
+      read -p "A 90-touchpad.conf already exists in $X11CFGDIR, overwrite? [y,n]: " yn
+      case $yn in
+        [Yy]) cp ./90-touchpad.conf "$X11CFGDIR"; break;;
+        [Nn]) break;;
+        *) echo "Please answer 'y' or 'n'."
+      esac
+    done
+  fi
+fi
+
 
 
 echo "Installing FontAwesome..."
@@ -89,16 +108,22 @@ else
 fi
 
 
-echo "Installing scripts..."
-if ! [ -d "$SCRIPTPATH" ]
-then
-  cp -r ./scripts $HOME
-else
-  if ! [ -f "$SCRIPTPATH/gpmdp_i3.sh" ]
-  then
-    cp .scripts/gpmdp_i3.sh "$SCRIPTPATH/"
-  fi
-fi
+#echo "Installing scripts..."
+#if ! [ -d "$SCRIPTPATH" ]
+#then
+#  cp -r ./scripts $HOME
+#else
+#  if ! [ -f "$SCRIPTPATH/gpmdp_i3.sh" ]
+#  then
+#    cp .scripts/gpmdp_i3.sh "$SCRIPTPATH/"
+#  fi
+#fi
+
+echo "Creating sudoers file for passwordless execution of brightnessctl..."
+sudo echo "$(whoami) ALL= NOPASSWD: /usr/bin/brightnessctl" > /etc/sudoers.d/brightnessctl
 
 echo "Reloading i3..."
 i3-msg restart
+
+echo "Please logout and login to apply the X11 config"
+echo "Enjoy!"
